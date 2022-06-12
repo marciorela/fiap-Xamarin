21f6e6689acd7a3000ac94a89081a0ae15f93307
Marcio Rela <marciorela@hotmail.com>
2022-06-12 11:32:07 -0300

    Exemploes de Maps e QRCode

---
+++ XFMapsFiap/XFMapsFiap/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFMapsFiap
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MapaComunClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync( new View.MapaComunView());
+        }
+
+        private void LocalizacaoClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View.LocalizacaoView());
+        }
+
+        private void ZoomClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View.ZoomView());
+        }
+
+        private void BuscaClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View.BuscaView());
+        }
+
+        private void PinClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View.PinView());
+        }
+
+    }
+}
